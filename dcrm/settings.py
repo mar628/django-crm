16e4ff0456822ad851cf388179305e320f298e58
steve mark <stevenmaithya42@gmail.com>
2023-08-18 12:31:16 +0300

    Initial Commit

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -25,8 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
-]
+    'website',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- website/views.py
+++ website/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+
+def home (request):
+    #checking to see if log in
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in, Please try Again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    logout(request,)
+    messages.success(request, "you have been logged out...")
+    return redirect('home')
+
+def logout_user(request):
+    logout(request,)
+    messages.success(request, "you have been logged out...")
+    return redirect('home')
+
 
 # Create your views here.
+def register_user(request):
+    return render(request, 'register.html', {})
